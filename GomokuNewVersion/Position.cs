bc8a634e5a934813be4d7466e4618de2024d15e2
fanjae <hjjang45@gmail.com>
2026-05-09 20:28:17 +0900

    Refactor 좌표값 구조체 분리

---
+++ GomokuNewVersion/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace GomokuNewVersion
+{
+    readonly struct Position
+    {
+        public int Row { get; }
+        public int Col { get; }
+
+        public Position(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+    }
+}
