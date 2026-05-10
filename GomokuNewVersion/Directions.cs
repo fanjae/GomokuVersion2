b8ac5ca4180ff07764d22535d5a2e4897aaf77e4
fanjae <hjjang45@gmail.com>
2026-05-10 14:12:51 +0900

    Refactor : 방향을 공용으로 사용할 수 있게 분리

---
+++ GomokuNewVersion/Directions.cs
@@ -0,0 +1,30 @@
+﻿namespace GomokuNewVersion
+{
+    internal static class GomokuDirections // 이동 방향값 정보
+    {
+        private static readonly int[,] Directions = 
+        {
+            {0 ,1},
+            {1, 0},
+            {1, 1},
+            {1,-1}
+        };
+
+        public static int GetRow(int index)
+        {
+            ValidateIndex(index);
+            return Directions[index, 0];
+        }
+
+        public static int GetCol(int index)
+        {
+            ValidateIndex(index);
+            return Directions[index, 1];
+        }
+        private static void ValidateIndex(int index) // 인덱스 넘어간 경우 예외처리
+        {
+            if (index < 0 || index >= 4)
+                throw new ArgumentOutOfRangeException(nameof(index), "방향 인덱스가 범위를 벗어났습니다.");
+        }
+    }
+}
